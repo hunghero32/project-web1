fbfc1092aa64b0f9c4659201f6662b669e62868b
Long LH <luonghoanglong.3008@gmail.com>
2023-11-13 23:05:27 +0700

    sửa tên file pdo, xóa 1 số file rỗng

--- model/corp/m_corp.php
+++ model/corp/m_corp.php
@@ -1,6 +1,7 @@
 <?php
-include_once('../pdo.php');
-class M_corp extends pdo
+// include_once('../..c_pdo.php');
+include('../c_pdo.php');
+class M_corp extends db
 {
     function get_all_Corp($name, $major, $address)
     {
@@ -9,16 +10,16 @@ class M_corp extends pdo
                 FROM corp c
                 INNER JOIN user u ON c.idcorp = u.id
                 WHERE u.role = 3";
-
-        $sql .= $name !== '' ? " AND c.name like '$name' " : "";
-        $sql .= $major !== '' ? " AND c.major like '$major' " : "";
-        $sql .= $address !== '' ? " AND c.address like '$address' " : "";
-
-        $sql .= "GROUP BY c.id, userRole
-                ORDER BY c.id DESC";
-
+    
+        $sql .= $name !== '' ? " AND c.name LIKE '$name' " : "";
+        $sql .= $major !== '' ? " AND c.major LIKE '$major' " : "";
+        $sql .= $address !== '' ? " AND c.address LIKE '$address' " : "";
+    
+        $sql .= " GROUP BY c.id, userRole ORDER BY c.id DESC";
+    
         return $this->pdo_query($sql, []);
     }
+    
 
     function get_one_Corp($id)
     {
@@ -29,7 +30,7 @@ class M_corp extends pdo
         return $this->pdo_query_one($sql);
     }
 
-    // UPDATE user as u inner join corp as c
+    // UPDATE user as u, corp as c
     //     SET u.username = 'long',
     //         u.img = 'img.img',
     //         u.email = 'longlhph31572@fpt.edu.vn',
@@ -37,35 +38,21 @@ class M_corp extends pdo
     //         u.address = 'Đà Nẵng',
     //         c.exp = '10',
     //         c.major = 'be',
-    //         c.description = 'test query 20 table'
+    //         c.description = 'test query 2 table'
     //     WHERE u.id = '7' AND c.idcorp = '7'
 
     function update_Corp($id, $name, $img, $email, $phone, $address, $exp, $major, $desc)
     {
-        $sql = "UPDATE user AS u
-                INNER JOIN corp AS c ON u.id = c.idcorp
-                SET u.username = :name,
-                    u.img = :img,
-                    u.email = :email,
-                    u.phone = :phone,
-                    u.address = :address,
-                    c.exp = :exp,
-                    c.major = :major,
-                    c.description = :desc
-                WHERE u.id = :id AND c.idcorp = :id";
-
-        $stmt = $this->prepare($sql);
-
-        $stmt->bindParam(':name', $name);
-        $stmt->bindParam(':img', $img);
-        $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':phone', $phone);
-        $stmt->bindParam(':address', $address);
-        $stmt->bindParam(':exp', $exp);
-        $stmt->bindParam(':major', $major);
-        $stmt->bindParam(':desc', $desc);
-        $stmt->bindParam(':id', $id);
-
-        $stmt->execute();
+        $sql = "UPDATE user as u, corp as c
+        SET u.username = '$name',
+            u.img = '$img',
+            u.email = '$email',
+            u.phone = '$phone',
+            u.address = '$address',
+            c.exp = '$exp',
+            c.major = '$major',
+            c.description = '$desc'
+        WHERE u.id = '7' AND c.idcorp = '$id'";
+        $this->pdo_execute($sql);
     }
 }
