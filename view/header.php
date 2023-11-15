4f9b15a81d080a0434cc5af3095010585a424a31
Long LH <luonghoanglong.3008@gmail.com>
2023-11-15 07:27:11 +0700

    sửa lại điều hướng trang ds nhà tuyển dụng

--- view/header.php
+++ view/header.php
@@ -37,7 +37,7 @@
     <div class="navbar-area fixed-top">
 
         <div class="mobile-nav">
-            <a href="index.html" class="logo">
+            <a href="index.php" class="logo">
                 <script type="text/javascript" style="display:none">
                     window.__mirage2 = {
                         petok: "R5hvAGvuYYzQ_tuA6uW2YWY70c9VahuBUEONJqSdIJI-1800-0"
@@ -51,35 +51,35 @@
         <div class="main-nav three">
             <div class="container-fluid">
                 <nav class="navbar navbar-expand-md navbar-light">
-                    <a class="navbar-brand" href="index.html">
+                    <a class="navbar-brand" href="index.php">
                         <img data-cfsrc="assets/img/logo-three.png" alt="Logo" style="display:none;visibility:hidden;"><noscript><img src="assets/img/logo-three.png" alt="Logo"></noscript>
                     </a>
                     <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
                         <ul class="navbar-nav">
                             <li class="nav-item">
                                 <span class="tooltip-span">Hot</span>
-                                <a href="jobs.html" class="nav-link dropdown-toggle">Việc làm
+                                <a href="#" class="nav-link dropdown-toggle">Việc làm
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a href="employers.html">Nhà tuyển dụng</a>
+                                <a href="index.php?act=listCorp">Nhà tuyển dụng</a>
                             </li>
                             <li class="nav-item">
-                                <a href="candidates.html">CV</a>
+                                <a href="#">CV</a>
                             </li>
                             <li class="nav-item">
-                                <a href="about.html" class="nav-link">Giới thiệu</a>
+                                <a href="#" class="nav-link">Giới thiệu</a>
                             </li>
                             <li class="nav-item">
-                                <a href="contact.html" class="nav-link">Liên hệ</a>
+                                <a href="#" class="nav-link">Liên hệ</a>
                             </li>
                         </ul>
                         <div class="side-nav three">
-                            <a class="login-left" href="login.html">
+                            <a class="login-left" href="#">
                                 <i class="flaticon-enter"></i>
                                 Đăng kí / Đăng nhập
                             </a>
-                            <a class="job-right" href="post-a-job.html">
+                            <a class="job-right" href="#">
                                 Tuyển dụng
                                 <i class="bx bx-plus"></i>
                             </a>
