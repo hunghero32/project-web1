d14f32f68b9c7d9b3db140e21ce134438197ed25
dungnt0605 <sdvndung051230@gmail.com>
2023-11-16 19:40:43 +0700

    feat: signin signup

--- controller/user.php
+++ controller/user.php
@@ -1,12 +1,42 @@
 <?php
 
+
 switch ($act) {
     case 'signin':
+        if (isset($_POST['signin'])) {
+            $user = login($_POST['username'], $_POST['pass']);
+            if (is_array($user)) {
+                $_SESSION['username'] = $user;
+                header('Location: index.php');
+                echo "<script>alert('Login successful!');</script>";
+            } else {
+                echo "<script>alert('Login successful!');</script>";
+                include "view/user/signin.php";
+            }
+        }
+
         include 'view/user/signin.php';
         break;
 
     case 'signup':
-        include 'view/user/signup.php';
+        if (isset($_POST['submit']) ) {
+            $username = $_POST['username'];
+            $image = $_FILES['img'];
+            $avatar = $image['name']; 
+            $pass = $_POST['pass'];
+            $phone = $_POST['phone'];
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $adress = $_POST['adress'];
+            $role = $_POST['role'];
+            $target_file = 'assets/img/' . $avatar;
+            move_uploaded_file($image['tmp_name'], $target_file);
+
+            add_user($username, $pass,$name , $avatar , $email, $phone, $adress , $role);
+
+            $thongbao = "Đăng ký thành công Vui lòng đăng nhập";
+        } 
+        include "view/user/signup.php";
         break;
 
     case 'forgotpassword':
