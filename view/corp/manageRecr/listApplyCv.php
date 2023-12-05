eab9dc5683dc720d598df75ac9907f4c473729de
dungnt0605 <sdvndung051230@gmail.com>
2023-12-05 14:45:08 +0700

    feat : browse cv

--- view/corp/manageRecr/listApplyCv.php
+++ view/corp/manageRecr/listApplyCv.php
@@ -1,4 +1,4 @@
-<div class="tab-pane fade  <?= (!isset($_GET['id']) && !isset($_GET['idEdit']) && !isset($_GET['idInfo']) && !isset($_GET['profile']) && isset($_SESSION['same']) ) ? 'show active' : '' ?> " id="v-pills-apply" role="tabpanel" aria-labelledby="v-pills-apply-tab">
+<div class="tab-pane fade  <?= (!isset($_GET['id']) && !isset($_GET['idEdit']) && !isset($_GET['idInfo']) && !isset($_GET['profile']) && isset($_SESSION['same'])) ? 'show active' : '' ?> " id="v-pills-apply" role="tabpanel" aria-labelledby="v-pills-apply-tab">
     <form action="index.php?act=manage_recr" method="post" class="mb-5 d-flex justify-content-between align-items-center">
         <div class="d-flex align-items-center">
             <input type="search" name="kym" class="p-2 px-5 rounded-start border border-secondary me-3" placeholder="Tìm kiếm ...">
@@ -17,46 +17,49 @@
             </div>
             <button class="btn btn-info rounded-end p-2 mx-3 text-white" name="submit" style="background-color: var(--secondary);">Tìm kiếm</button>
         </div>
-       
+
 
     </form>
-    <?php if (empty($listApply) ) { ?>
-                <div class=" my-3">
-                    <div class="employer-item text-center">
-                        Không có dữ liệu tìm thấy !
+    <?php if (empty($listApply)) { ?>
+        <div class=" my-3">
+            <div class="employer-item text-center">
+                Không có dữ liệu tìm thấy !
+            </div>
+        </div>
+        <?php } else {
+        foreach ($listApply as $cv) {
+            extract($cv) ?>
+
+            <div class="candidate-item two" style="cursor:pointer;" onclick="return window.location.href='index.php?act=infoCv&id=<?= $iduser ?>'">
+                <div class="left">
+                    <h2><a href="index.php?act=infoCv&id=<?= $iduser ?>" class="text-dark"><?= $namecv ?></a></h2>
+                    <span class="w-100">Công việc ứng tuyển : <?= $job ?></span>
+                    <ul class="experience">
+                        <li>Kinh nghiệm: <span><?= $exp ?> Tháng</span></li>
+                        <li>Mức lương: <span><?= $salary ?></span></li>
+                        <li>
+                            <i class="flaticon-send"></i> <?= $address ?>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li><?= $progLang ?></li>
+                    </ul>
+                    <div class="d-flex justify-content-between align-content-center">
+                        <div class="cmn-link">
+                            <a href="index.php?act=infoCv&id=<?= $iduser ?>">
+                                <i class="flaticon-right-arrow one"></i> Xem Chi Tiết
+                                <i class="flaticon-right-arrow two"></i>
+                            </a>
+                        </div>
+                        <a href="index.php?act=addCV&idinfo=<?=$idinfo?>" class="btn " style="background-color: var(--secondary);">Xét duyệt</a>
                     </div>
                 </div>
-                <?php } else {
-                foreach ($listApply as $cv) {
-                    extract($cv) ?>
-                    
-                        <div class="candidate-item two" style="cursor:pointer;" onclick="return window.location.href='index.php?act=infoCv&id=<?= $iduser ?>'">
-                            <div class="left">
-                                <h2><a href="index.php?act=infoCv&id=<?= $iduser ?>" class="text-dark"><?= $namecv ?></a></h2>
-                                <span class="w-100">Công việc ứng tuyển : <?= $job ?></span>
-                                <ul class="experience">
-                                    <li>Kinh nghiệm: <span><?= $exp ?> Tháng</span></li>
-                                    <li>Mức lương: <span><?= $salary ?></span></li>
-                                    <li>
-                                        <i class="flaticon-send"></i> <?= $address ?>
-                                    </li>
-                                </ul>
-                                <ul>
-                                    <li><?= $progLang ?></li>
-                                </ul>
-                                <div class="cmn-link">
-                                    <a href="index.php?act=infoCv&id=<?= $iduser ?>">
-                                        <i class="flaticon-right-arrow one"></i> Xem Chi Tiết
-                                        <i class="flaticon-right-arrow two"></i>
-                                    </a>
-                                </div>
-                            </div>
-                            <img data-cfsrc="<?= checkUserCvNull($avatarCv) ?>" alt="<?= $name ?>" style="display:none;visibility:hidden; <?= $avatarCv == '' ? 'width: 80px;height: 80px' : '' ?>">
-                            <noscript><img style="<?= $avatar == '' ? 'width: 80px;height: 80px' : '' ?>" src="<?= checkUserCvNull($avatar) ?>" alt="Candidate"></noscript>
-                        </div>
-                    
-                <?php } ?>
-            <?php } ?>
+                <img data-cfsrc="<?= checkUserCvNull($avatarCv) ?>" alt="<?= $name ?>" style="display:none;visibility:hidden; <?= $avatarCv == '' ? 'width: 80px;height: 80px' : '' ?>">
+                <noscript><img style="<?= $avatar == '' ? 'width: 80px;height: 80px' : '' ?>" src="<?= checkUserCvNull($avatar) ?>" alt="Candidate"></noscript>
+            </div>
+
+        <?php } ?>
+    <?php } ?>
     <div class="pagination-area">
         <ul class="pagination">
             <li><a href="#" id="prev">Prev</a></li>
